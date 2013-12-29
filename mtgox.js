0c4b3db205efefda5b3c2fac301c99dd8f68252f
Jason Ihde <jason@quantsig.net>
2013-12-29 12:02:37 +0000

    Comments, license

--- mtgox.js
+++ mtgox.js
@@ -1,29 +1,13 @@
 /*jshint globalstrict: true*/
 /*jshint node: true*/
 /*jshint browser: true*/
-/*!
+/**@preserve
 QSN MtGox Javascript Websocket Client for Node.js and HTML5 Browsers.
 
-Copyright (c) 2013 Quantitative Signals Network. https://www.quantsig.net
-Distributed under the terms of The MIT License (MIT).
+Copyright (c) 2013 Quantitative Signals Network (https://www.quantsig.net).
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
+Distributed under the terms of the MIT License (http://opensource.org/licenses/MIT).
 
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
 */
 "use strict";
 function GoxClient(conf) {
